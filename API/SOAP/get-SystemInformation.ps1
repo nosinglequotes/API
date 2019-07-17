45a51280618bfbaad18c775f6c3e37a2eeb0db52
Jeff Caten <jeff_caten@trendmicro.com>
2019-07-17 10:44:43 -0500

    Removed unused comments. Added  variable for output file

--- API/SOAP/get-SystemInformation.ps1
+++ API/SOAP/get-SystemInformation.ps1
@@ -4,6 +4,9 @@ param (
     [Parameter(Mandatory=$false)][string]$tenant
 )
 
+# Variables
+$file = ".\systemInformation_$date.csv"
+
 ##################### Authentication Block Start #####################
 $passwordinput = Read-host "Password for Deep Security Manager" -AsSecureString
 $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordinput))
@@ -29,18 +32,8 @@ catch {
 
 $systemInformation = $DSM.systemInformationRetrieve($SID)
 
-<#
-#Write-Host $systemInformation.key
-Write-host "Loop Started"
-foreach ($i in $systemInformation.key) {
-	$response = $systemInformation | Select-Object key, name, value
-	$response | Export-Csv -Path systemInformation.csv -Append -NoTypeInformation
-}
-Write-host "Loop Finished"
-#>
-
 $response = $systemInformation | Select-Object key, name, value
-$response | Export-Csv -Path systemInformation.csv -Append -NoTypeInformation
+$response | Export-Csv -Path $file -Append -NoTypeInformation
 
 # Log out
 $DSM.endSession($SID)
