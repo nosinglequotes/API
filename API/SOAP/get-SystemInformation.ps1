109268dbeecc79094996055db322938ca0082f1b
Jeff Caten <jeff_caten@trendmicro.com>
2019-07-17 11:26:06 -0500

    Removed  param because this information can't be pulled from tN

--- API/SOAP/get-SystemInformation.ps1
+++ API/SOAP/get-SystemInformation.ps1
@@ -1,7 +1,6 @@
 param (
     [Parameter(Mandatory=$true, HelpMessage="FQDN and port for Deep Security Manager; ex dsm.example.com:443--")][string]$manager,
     [Parameter(Mandatory=$true, HelpMessage="DeepSecurity Manager Username with api access--")][string]$user,
-    [Parameter(Mandatory=$false)][string]$tenant
 )
 
 # Variables
